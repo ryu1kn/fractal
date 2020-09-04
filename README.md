9d478317f43ba9cb1169474e068739811f550431
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-09-04 15:05:24 +1000

    Introduced `besides`

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Fractal
+
+## References
+
+* [3A: Henderson Escher Example - MIT Open courseware](https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-001-structure-and-interpretation-of-computer-programs-spring-2005/video-lectures/3a-henderson-escher-example/)
--- src/fractal/core.clj
+++ src/fractal/core.clj
@@ -7,6 +7,11 @@
 (defn load-image [image-path] (MarvinImageIO/loadImage image-path))
 (defn save-image [image image-path] (MarvinImageIO/saveImage image image-path))
 
+(defn pos [scale orig-size orig-pos]
+  (+ (* scale orig-size)
+     (* (- 1 scale) orig-pos)
+     -1))
+
 (defn rotate [image]
   (let [width'  (.getHeight image)
         height' (.getWidth image)
@@ -17,6 +22,21 @@
         (.setIntColor image' w h (.getIntColor image (- height' h 1) w)))
       image')))
 
+(defn beside [image1 image2 factor]
+  (let [width'  (.getWidth image1)
+        height' (.getHeight image1)
+        image'  (MarvinImage. width' height')]
+    (do
+      (doseq [w (range width')
+              h (range height')]
+        (.setIntColor image' (* factor w) h (.getIntColor image1 w h)))
+      (doseq [w (range width')
+              h (range height')]
+        (.setIntColor image' (pos factor width' w) h (.getIntColor image2 w h)))
+      image')))
+
+(defn p [image] (beside image image 0.3))
+
 (defn -main [& args]
   (let [image (load-image "./test.png")]
-    (save-image (rotate image) "test-output.png")))
+    (save-image (p image) "test-output.png")))
